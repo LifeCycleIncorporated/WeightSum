290c5b0d1228b72d6f5eeaac226ca716d03d1404
Asadu <techasadbd@gmail.com>
2023-08-24 20:49:30 +0600

    Java

--- app/src/main/java/com/example/weightsum/MainActivity.java
+++ app/src/main/java/com/example/weightsum/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.weightsum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button1, button2, button3, button4, button5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.oneButtonId);
+        button2 = findViewById(R.id.twoButtonId);
+        button3 = findViewById(R.id.threeButtonId);
+        button4 = findViewById(R.id.fourButtonId);
+        button5 = findViewById(R.id.fiveButtonId);
+
+
     }
 }
